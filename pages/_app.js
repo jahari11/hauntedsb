d2bbcc81a804d62338c8e84c20fba8fccb098237
Jahari <jahari.crawford.young@gmail.com>
2024-07-07 22:56:05 -0400

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import "@/styles/globals.css";
 
+import { Bodoni_Moda } from "next/font/google";
+
+const Moda = Bodoni_Moda({ subsets: ["latin"] });
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${Moda.className}`}>
+      <Component {...pageProps} />
+  </main>
+  );
+  
 }
